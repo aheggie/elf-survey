24385c3c09cbdf8d4ce210f653b1fee83f12a0c2
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-13 18:46:28 -0500

    center forest on origin

--- src/Trees.tsx
+++ src/Trees.tsx
@@ -1,6 +1,6 @@
 import Tree from "./Tree";
 
-const dummyTreeMap = [
+const treeMatrix = [
   [
     { height: 1 },
     { height: 0 },
@@ -55,13 +55,20 @@ const mergeKeys = (key1: number, key2: number): string =>
   `${key1.toString()}${key2.toString()}`;
 
 const Trees = () => {
+  // these are to center the center point at the origin
+  //
+  // rowOffest in particular presumes that the matrix is square and takes the first row length
+  // as standard - could be calculated dynamically in the function below
+  const rowOffset = 0.5 - treeMatrix[0].length / 2;
+  const colOffset = 0.5 - treeMatrix.length / 2;
+  console.log(rowOffset, colOffset);
   return (
     <>
-      {dummyTreeMap.map((row, rowIdx) =>
+      {treeMatrix.map((row, rowIdx) =>
         row.map(({ height }, colIdx) => (
           <Tree
             height={height}
-            position={[rowIdx, 0, colIdx]}
+            position={[rowIdx + rowOffset, 0, colIdx + colOffset]}
             key={mergeKeys(rowIdx, colIdx)}
           />
         ))
