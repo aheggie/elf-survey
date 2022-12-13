1fc68f99425e3ef92a968fb23ad4a23d972f75b3
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-12 19:16:57 -0500

    some kind of scene is loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Canvas camera={{ position: [2, 4, 10] }} shadows>
+        <axesHelper args={[10]} />
+        <gridHelper args={[10, 10]} />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
