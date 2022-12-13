f474009a5a67405acf23f4586014f493c5808852
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-12 19:49:46 -0500

    add stats and orbital controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Canvas } from "@react-three/fiber";
-
+import { OrbitControls, Stats } from "@react-three/drei";
 import "./App.css";
+
+const testing = false;
 
 function App() {
   return (
     <div className="App">
       <Canvas camera={{ position: [2, 4, 10] }} shadows>
+        {testing ? <Stats /> : null}
         <axesHelper args={[10]} />
         <gridHelper args={[10, 10]} />
+        <OrbitControls />
       </Canvas>
     </div>
   );
