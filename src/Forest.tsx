653c304452833a056fb6076e9d1487f28f9a0773
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-13 20:44:02 -0500

    it all loads in! but its too slow

--- src/Forest.tsx
+++ src/Forest.tsx
@@ -1,56 +1,56 @@
 import Tree from "./Tree";
 import { TreeObj } from "./utilities/processForestString";
 
-const treeMatrix2 = [
-  [
-    { height: 1 },
-    { height: 0 },
-    { height: 3 },
-    { height: 2 },
-    { height: 4 },
-    { height: 3 },
-  ],
-  [
-    { height: 2 },
-    { height: 4 },
-    { height: 3 },
-    { height: 2 },
-    { height: 0 },
-    { height: 4 },
-  ],
-  [
-    { height: 2 },
-    { height: 0 },
-    { height: 4 },
-    { height: 1 },
-    { height: 0 },
-    { height: 3 },
-  ],
-  [
-    { height: 2 },
-    { height: 4 },
-    { height: 3 },
-    { height: 2 },
-    { height: 0 },
-    { height: 4 },
-  ],
-  [
-    { height: 2 },
-    { height: 0 },
-    { height: 4 },
-    { height: 1 },
-    { height: 0 },
-    { height: 3 },
-  ],
-  [
-    { height: 1 },
-    { height: 0 },
-    { height: 3 },
-    { height: 2 },
-    { height: 4 },
-    { height: 3 },
-  ],
-];
+// const treeMatrix2 = [
+//   [
+//     { height: 1 },
+//     { height: 0 },
+//     { height: 3 },
+//     { height: 2 },
+//     { height: 4 },
+//     { height: 3 },
+//   ],
+//   [
+//     { height: 2 },
+//     { height: 4 },
+//     { height: 3 },
+//     { height: 2 },
+//     { height: 0 },
+//     { height: 4 },
+//   ],
+//   [
+//     { height: 2 },
+//     { height: 0 },
+//     { height: 4 },
+//     { height: 1 },
+//     { height: 0 },
+//     { height: 3 },
+//   ],
+//   [
+//     { height: 2 },
+//     { height: 4 },
+//     { height: 3 },
+//     { height: 2 },
+//     { height: 0 },
+//     { height: 4 },
+//   ],
+//   [
+//     { height: 2 },
+//     { height: 0 },
+//     { height: 4 },
+//     { height: 1 },
+//     { height: 0 },
+//     { height: 3 },
+//   ],
+//   [
+//     { height: 1 },
+//     { height: 0 },
+//     { height: 3 },
+//     { height: 2 },
+//     { height: 4 },
+//     { height: 3 },
+//   ],
+// ];
 
 const mergeKeys = (key1: number, key2: number): string =>
   `${key1.toString()}${key2.toString()}`;
@@ -61,12 +61,12 @@ const Forest: React.FC<{ treeMatrix: TreeObj[][] }> = ({ treeMatrix }) => {
   //
   // rowOffest in particular presumes that the matrix is square and takes the first row length
   // as standard - could be calculated dynamically in the function below
-  const rowOffset = 0 - (treeMatrix2[0].length + 1) / 2;
-  const colOffset = 0 - (treeMatrix2.length + 1) / 2;
+  const rowOffset = 0 - (treeMatrix[0].length + 1) / 2;
+  const colOffset = 0 - (treeMatrix.length + 1) / 2;
   console.log(rowOffset, colOffset);
   return (
     <>
-      {treeMatrix2.map((row, rowIdx) =>
+      {treeMatrix.map((row, rowIdx) =>
         row.map(({ height }, colIdx) => (
           <Tree
             height={height}
